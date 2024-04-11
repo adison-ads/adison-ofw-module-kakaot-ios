d5f2407463d58e6489f664aa6e4038d12f3b43a8
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-04-11 17:40:12 +0900

    0.10.0 릴리즈

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,16 +12,16 @@ let package = Package(
             targets: ["KakaoTOfwModule"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: 3.5.0)
-    ]
+        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: "3.5.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "KakaoTOfwModule",
-            url: "https://github.com/adison-ads/adison-ofw-module-kakaot-ios/release/download/0.10.0/KakaoTOfwModule.zip",
-            checksum: ""
-        )
+            url: "https://github.com/adison-ads/adison-ofw-module-kakaot-ios/releases/download/0.10.0/KakaoTOfwModule.zip",
+            checksum: "1e59abe314ce432f3bc0aef9dcf739026e8f46e4ed53f822dd73310ffbf26271"
+        ),
         .target(
             name: "KakaoTOfwModuleTarget",
             dependencies: [.product(name: "AdisonOfferwallSDK", package: "adison-offerwall-ios-sdk")],
